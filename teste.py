916c1233be435e34694858bad177bba7ce2859ec
juliaalcantaraft <julia.alcantaraft@gmail.com>
2026-04-28 15:20:17 -0300

    Update teste.py
    imagepraia

--- teste.py
+++ teste.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title('Teste ECMI 2')
 st.write("Esse é o meu texto")
+
+st.image('https://conhecimentocientifico.r7.com/wp-content/uploads/2020/09/praias-o-que-sao-caracteristicas-e-importancia-para-o-meio-ambiente-5.jpg.webp')
