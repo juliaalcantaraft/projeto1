c500ddb08b896c81b0706a6dcdd5d0f5c12b9d73
juliaalcantaraft <julia.alcantaraft@gmail.com>
2026-04-28 15:39:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,4 +3,11 @@ import streamlit as st
 st.title('Teste ECMI 2')
 st.write("Esse é o meu texto")
 
+nome = st.text_input('Digite o seu nome')
+if nome:
+  t.write(nome, 'é um cara legal!')
+
+
+
 st.image('https://conhecimentocientifico.r7.com/wp-content/uploads/2020/09/praias-o-que-sao-caracteristicas-e-importancia-para-o-meio-ambiente-5.jpg.webp')
+
